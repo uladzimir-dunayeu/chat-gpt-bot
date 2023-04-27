d54222a90cdb7459e67efe9c8920630dc8dc2c7f
v.dunaev <vladimir.dunaev@efinancialcareers.com>
2023-04-27 17:20:50 +0300

    add session

--- src/main.js
+++ src/main.js
@@ -1,27 +1,45 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, session } from 'telegraf';
 import { code } from 'telegraf/format'
 import { message } from 'telegraf/filters'
 import config from 'config'
 import { ogg } from './ogg.js';
 import { openai } from './openai.js';
 
+const INITIAL_SESSION = {
+    messages: [],
+}
+
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
+bot.use(session());
+
+bot.command('new', async (ctx) => {
+    ctx.session = INITIAL_SESSION;
+    await ctx.reply('Waiting for your voice message');
+})
+
+bot.command('start', async(ctx) => {
+    ctx.session = INITIAL_SESSION;
+    await ctx.reply('Waiting for your voice message');
+})
 
 bot.on(message('voice'), async (ctx) => {
+    ctx.session = ctx.session ? ctx.session : INITIAL_SESSION;
     try {
-        await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'));
+        await ctx.reply(code('Wait for a response from the server...'));
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+
         const userId = String(ctx.message.from.id);
         const oggPath = await ogg.create(link.href, userId);
         const mp3Path = await ogg.toMp3(oggPath, userId);
 
         const text = await openai.transcription(mp3Path);
-        
-        await ctx.reply(code(`Ваш запрос: ${text}`));
+        await ctx.reply(code(`Your request: ${text}`));
 
-        const messages = [{role: openai.roles.USER, content: text}];
-        const response = await openai.chat(messages);
+        ctx.session.messages.push({role: openai.roles.USER, content: text});
+        const response = await openai.chat(ctx.session.messages);
+        ctx.session.messages.push({role: openai.roles.ASSISTANT, content: response.content});
+
         await ctx.reply(response.content);
     } catch (e) {
         console.log('Error while voice message', e.message);
